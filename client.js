79e919e76d0ca43519480a11ba5c2bef8a7b4cd0
Adam Marsala <adammarsala@hotmail.com>
2021-02-10 14:39:52 +0000

    Move commands have been tested with setTimeout and setInterval

--- client.js
+++ client.js
@@ -11,12 +11,21 @@ const connect = function() {
 
   conn.on("connect", (data) => { // The "connect" event is triggered on a connection as soon as it is successfully established.
     console.log("Connection established XD");
-    conn.write("Name: AJM");
-  })
+    conn.write("Name: AJM"); // .write method lets you send an argument string to the server at anytime!
+  });
+
+  conn.on("connect", (data) => {
+    // movement upon connection, start loop
+    // for (let i = 0; i <= 10; i++) {
+    //   setInterval(() => {
+    //     conn.write("Move: up");
+    //   }, 500); 
+    // }
+  });
   
   conn.on("data", (data) => {
     console.log("Server says: " + data);
-  })
+  });
 
   return conn; // the object returned from net.createConnection({host,port})
 }
